026ff1b58b4b51f23e17bc600a4981fa117a67b6
github username <github email address>
2018-08-17 14:55:29 -0400

    styled navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,11 +4,12 @@ import { Link } from 'react-router-dom';
 class NavBar extends Component {
   render(){
     return(
-      <div className="navbar">
-
-        <Link to="/artwork">Artwork     </Link>
-        <Link to="/login">Login     </Link>
-        
+      <div className="div--nav-bar">
+        <Link className="nav-item logo div--nav-bar-item" to="/">Obra</Link>
+        <Link className="nav-item div--nav-bar-item" to="/artwork">Artwork</Link>
+        <Link className="nav-item div--nav-bar-item" to="/artwork">Annotations</Link>
+        <Link className="nav-item nav-login div--nav-bar-item" to="/login">Login</Link>
+        <Link className="nav-item nav-login div--nav-bar-item" to="/artwork">About</Link>
       </div>
     )
   }
