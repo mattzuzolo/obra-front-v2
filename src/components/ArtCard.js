f8464f8be7d2b4458e04202bf8c50caf0362f0e1
github username <github email address>
2018-08-18 11:39:15 -0400

    organized ArtCard code

--- src/components/ArtCard.js
+++ src/components/ArtCard.js
@@ -8,6 +8,7 @@ class ArtCard extends Component {
         <div className="div--art-card-left">
           <img src={this.props.primaryimageurl} alt="thumbail"></img>
         </div>
+        
         <div className="div--art-card-right">
           <h1 className="h1 art-card-title">{this.props.title}</h1>
             { this.props.artist
@@ -17,8 +18,6 @@ class ArtCard extends Component {
           <p className="p art-card-detail"><text style={{ fontWeight: 'bold' }}>Medium:</text> {this.props.medium}</p>
           <p className="p art-card-detail"><text style={{ fontWeight: 'bold' }}>Culture of origin:</text> {this.props.culture}</p>
           <p className="p source-credit art-card-detail detail-source"><text style={{ fontWeight: 'bold' }}>Source:</text> <a href={this.props.url}>Harvard Art Museums</a> </p>
-
-
         </div>
 
       </div>
