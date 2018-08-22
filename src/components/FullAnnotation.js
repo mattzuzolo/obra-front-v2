236184bbd9a19fc56cc296e431467d74a320649f
github username <github email address>
2018-08-21 21:57:09 -0400

    FullAnnotation only displays edit button for the annotations owner.

--- src/components/FullAnnotation.js
+++ src/components/FullAnnotation.js
@@ -1,20 +1,36 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
+import { connect } from 'react-redux';
+
 
 class FullAnnotation extends Component {
   render(){
+    console.log("FullAnnotation props —— USER", this.props.selectedAnnotation.user[0])
+    console.log("FullAnnotation props —— USER", this.props.loggedInUser._id)
     return(
       <div className="div div--full-annotation">
         <h1>{this.props.selectedAnnotation.headline}</h1>
         <p>{this.props.selectedAnnotation.content}</p>
         <p>Source: <a href={this.props.selectedAnnotation.source}>{this.props.selectedAnnotation.source}</a></p>
 
-        <button className="button button--annotation-card button--annotation-card-update" onClick={(event) => this.props.onAnnotationUpdateFormDisplay(event)} >Update Annotation</button>
+          { (this.props.selectedAnnotation.user[0] === this.props.loggedInUser._id)
+                    ?
+                    <Fragment>
+                      <button className="button button--annotation-card button--annotation-card-update" onClick={(event) => this.props.onAnnotationUpdateFormDisplay(event)} >Update Annotation</button>
+                      <button className="button button--annotation-card button--annotation-card-delete" onClick={(event) => this.props.onAnnotationCardDelete(event, this.props.selectedAnnotation)} >Delete Annotation</button>
+                    </Fragment>  
+                    : null
+          }
 
-        <button className="button button--annotation-card button--annotation-card-delete" onClick={(event) => this.props.onAnnotationCardDelete(event, this.props.selectedAnnotation)} >Delete Annotation</button>
       </div>
     );
   }
 
 }
 
-export default FullAnnotation;
+function mapStateToProps(state){
+  return {
+    loggedInUser: state.loggedInUser,
+  }
+}
+
+export default connect(mapStateToProps)(FullAnnotation);
